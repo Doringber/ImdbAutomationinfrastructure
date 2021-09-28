d4f8f0c9b7b3b68e2786ca20f3cc9ac1952e8814
doringber123 <dori@pango.co.il>
2021-09-28 13:45:16 +0300

    fix thing for docker2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome import options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
 def chrome_browser():
-    browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",desired_capabilities=DesiredCapabilities.CHROME)
+    # browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.implicitly_wait(5)
     browser.maximize_window()
     return browser
