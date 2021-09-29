6b63fb974fe04f5dd407027cc4c2fbc8e51bf787
doringber123 <dori@pango.co.il>
2021-09-29 23:02:33 +0300

    pop2

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,15 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope='function')
 def chrome_browser():
+
+    options = Options()
+    options.add_argument("--headless")
     browser = webdriver.Remote(command_executor="http://localhost:4444/wd/hub",desired_capabilities=DesiredCapabilities.CHROME)
     # browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.implicitly_wait(5)
