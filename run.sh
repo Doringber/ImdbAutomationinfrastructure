4c1d85b03e1b7627b6f0e3c0e5795c90122e8e9f
doringber123 <dori@pango.co.il>
2021-09-29 22:17:15 +0300

    bla

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+echo "run docker compose"
+docker-compose -f docker-compose-v3.yml up
+
+echo "run requirements"
+pip install -r ./requirements.txt
+
+echo "start run pytest"
 pytest -m smoke
